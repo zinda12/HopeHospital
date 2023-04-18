b33a3f971b0274b021db8b8bede4b697f84e3fc7
zinda12 <zin00aleksa@gmail.com>
2023-04-18 20:00:50 +0200

    fix

--- src/HospitalAPI/Controllers/PublicApp/DoctorController.cs
+++ src/HospitalAPI/Controllers/PublicApp/DoctorController.cs
@@ -34,36 +34,6 @@ namespace HospitalAPI.Controllers.PublicApp
             return Ok(_mapper.Map<List<DoctorDTO>>(_doctorService.GetAllGeneralPracticioners()));
         }
 
-        /*[HttpGet("availableGeneralPracticioners")]
-        public ActionResult GetAllAvailableGeneralPracticioners()
-        {
-            var generalPracticioners=_doctorService.GetAllGeneralPracticioners().ToList();
-            var availableGeneralPracticioners = new List<Doctor>();
-            var patients = new List<Patient>();
-            int minPatientsNumber = 10;
-            Doctor doctorWithMinPatients = null;
-            foreach(Doctor doctor in generalPracticioners)
-            {
-                patients = _patientService.GetBySelectedDoctorId(doctor.Id);
-                if (minPatientsNumber > patients.Count()) {
-
-                    minPatientsNumber = patients.Count();
-                    doctorWithMinPatients = doctor;
-                }     
-            }
-            availableGeneralPracticioners.Add(doctorWithMinPatients);
-            generalPracticioners.Remove(doctorWithMinPatients);
-            foreach (Doctor doctor in generalPracticioners)
-            {
-                patients = _patientService.GetBySelectedDoctorId(doctor.Id);
-                if (minPatientsNumber >= patients.Count() - 2)
-                {
-                    availableGeneralPracticioners.Add(doctor);
-                }
-            }
-            return Ok(_mapper.Map<List<DoctorDTO>>(availableGeneralPracticioners));
-        }*/
-
         [HttpGet("specialization")]
         public ActionResult<List<DoctorDTO>> GetDoctorsBySpecialization([FromQuery]DoctorSpecialization specialization)
         {
